815cf9ab0b851839f0100f909843b0f1cde213dd
Sammy Kao <sammy.kao@tufts.edu>
2025-05-01 10:23:09 -0400

    TODO list now

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,26 +1,38 @@
 import projectsData from '../data/projects.json'
 import ProjectCard from '../components/project-card'
+import ideasData from '../data/ideas.json'
 
-export const metadata = {
-  title: 'Projects',
-  description: 'A showcase of my featured projects and technical work',
-}
 
 export default function ProjectsPage() {
   return (
     <section>
       <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
-        My Projects
+        My Projects + TODOs
       </h1>
       <div className="mb-8 prose prose-neutral dark:prose-invert">
         <p>
           I'm going to include my startup projects here too, rather than in the experiences page. 
           I don't sleep as much as I should, and this is why. A lot more AI projects on the bottom, if that's your jam.
-          I'm hoping to one day build a A2A + MCP style AI agent project, like RapidAPI but for AI agents, using
-          Google's A2A protocol. But time is a luxury I'm not sure I have.
+          If you like one of my TODOs, let me know! Or even better, build it and show me. I'm pretty open source with
+          ideas because at some point you realize the amount of execution arbitrage it really takes for something to 
+          be great.
         </p>
       </div>
-      
+      <h3 className="mb-4 text-2xl font-semibold tracking-tighter">
+        TODOs
+      </h3>
+      <div className="flex flex-col space-y-8"> 
+        {ideasData.map((idea, index) => (
+          <ProjectCard 
+            key={index}
+            title={idea.title} 
+            description={idea.description}
+          />
+        ))}
+      </div>
+      <h3 className="mb-4 text-2xl font-semibold tracking-tighter">
+        Projects
+      </h3>
       <div className="flex flex-col space-y-8">
         {projectsData.map((project, index) => (
           <ProjectCard 
